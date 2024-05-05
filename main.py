c73c871e859be58ea203b44438068b79df706e34
Amakye94 <163174506+Amakye94@users.noreply.github.com>
2024-05-05 18:39:12 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from Circle import Circle
+#from Address import Address
+
+#instantiate 2 classes address
+#hans_mier = Address("WorthStrasse", 8, 94032, "Passau")
+#Thomas_ank = Address("Romerplatz" , 2, 94032, "Passau")
+
+#print address
+#print(hans_mier.getAddress())
+#print(Thomas_ank.getAddress())
+
+Area_of_circle = Circle(int(input( )))
+print(Area_of_circle.get_Area_of_circle())
